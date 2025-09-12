f69fd86b7053e2f8481f6904c8930fc4a9b6e83f
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-09-12 14:41:49 +0530

    chore: dist updated

--- dist/index.js
+++ dist/index.js
@@ -25137,7 +25137,7 @@ async function validateSubscription() {
     try {
       await axios.get(API_URL, {timeout: 3000});
     } catch (error) {
-      if (error.response) {
+      if (error.response && error.response.status === 403) {
         console.error(
           'Subscription is not valid. Reach out to support@stepsecurity.io'
         );
@@ -25163,6 +25163,7 @@ async function run() {
   
 run();
   
+
 module.exports = __webpack_exports__;
 /******/ })()
 ;
